343657ea3db1113bd119c3892091e0c46590d390
Roman Efimov <romaonthego@gmail.com>
2013-10-29 10:07:46 -0500

    Add float.h

--- REMarkerClusterer/REMarkerClusterer.h
+++ REMarkerClusterer/REMarkerClusterer.h
@@ -26,6 +26,7 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
+#import <float.h>
 #import "REMarker.h"
 #import "RELatLngBounds.h"
 #import "RECluster.h"
